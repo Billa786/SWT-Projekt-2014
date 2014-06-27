bc2f3ca5d2210d09dfb184dc4c94cd105dff5aa6
Billa786 <Billa786@users.noreply.github.com>
2014-06-27 21:16:30 +0200

    Update Controller.cpp
    We have deleted the commented code and the parameters we hand over.

--- Controller.cpp
+++ Controller.cpp
@@ -1,17 +1,11 @@
 #include "Controller.h"
 #include "ObserverBar.h"
 #include "ObserverGraph.h"
-#include "ObserverLabel.h"
+#include "ObserverText.h"
 
-Controller::Controller(System::Windows::Forms::DataVisualization::Charting::Chart^ chartBar, System::Windows::Forms::DataVisualization::Charting::Chart^ chartGraph, System::Windows::Forms::Label^ labelTemp)
+Controller::Controller()
 {
 	subject = new Subject();
-	ObserverLabel^ observerlabel = gcnew ObserverLabel(labelTemp);
-	//subject->attach(*observerlabel);
-	//ObserverBar *observerbar = new ObserverBar(labelTemp);
-	//ObserverGraph *observergraph = new ObserverGraph();
-	//subject->attach(observerbar);
-	//subject->attach(observergraph);
 }
 
 Controller::~Controller()
@@ -19,15 +13,9 @@ Controller::~Controller()
 	delete subject;
 }
 
-void Controller::applyChanges(System::Windows::Forms::Label ^ labelLocation)
+void Controller::applyChanges()
 {
 	subject->download();
 	subject->parse();
 	subject->notify();
-	//string str = subject->arrWeather[0].location;
-	//System::String^ str2 = gcnew System::String(str.c_str());
-	//labelLocation->Text = gcnew System::String(str.c_str());
-
-
-	//labelLocation->Text = subject->arrWeather[0].location;
 }
