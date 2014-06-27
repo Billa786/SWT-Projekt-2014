45660f33842458d14779dd243408d26bb485c74e
Billa786 <Billa786@users.noreply.github.com>
2014-06-27 21:35:51 +0200

    Update Subject.cpp
    We have deleted the CurlPP includes because we dont use them to download the text file from the server we use WinInet.
    We have modified the the line where we insert structure to the list so we can use it for qwt.

--- Subject.cpp
+++ Subject.cpp
@@ -1,19 +1,8 @@
-#include "Subject.h"
-//#include "../curlpp/curlpp-0.7.3/include/curlpp/cURLpp.hpp"
-//#include <curlpp/cURLpp.hpp>
-//#include <curlpp/Easy.hpp>
-//#include <curlpp/Options.hpp>
-//#define CURL_STATICLIB
-//#include <curlpp\cURLpp.hpp>
-//#include <curlpp\Easy.hpp>
-//#include <curlpp\Types.hpp>
-#include <windows.h>
-#include <string>
-#include <urlmon.h>
-#pragma comment(lib,"urlmon.lib")
-#include <fstream>
 #include <iostream>
 using namespace std;
+#include <WinInet.h>
+//#pragma comment(lib, "Wininet.lib")
+#include "Weather.h"
 
 
 
@@ -30,28 +19,31 @@ void Subject::detach(Observer *observer)
 void Subject::notify()
 {
 	for(observer_list_iter = observer_list.begin(); observer_list_iter != observer_list.end(); observer_list_iter++)
-		(*observer_list_iter)->update();
+        (*observer_list_iter)->update();
+    cout << "called notify()" << endl;
 }
 
 void Subject::download()
 { 
 	/* Download data */
 	HRESULT hr;
-
-	hr = URLDownloadToFile(NULL,TEXT("http://tk-labor.iem.thm.de/bti-swt-pa-ss14/hochrechnungen.txt"), TEXT("D:\\test.txt"), 0, NULL);
+	DeleteUrlCacheEntry(TEXT("http://tk-labor.iem.thm.de/bti-swt-pa-ss14/hochrechnungen.txt"));
+    hr = URLDownloadToFile(NULL,TEXT("http://tk-labor.iem.thm.de/bti-swt-pa-ss14/hochrechnungen.txt"), TEXT("C:\\Users\\Qazi\\Desktop\\Temperaturdaten.txt"), 0, NULL);
 
     if(!SUCCEEDED(hr)) {
         string mMessage = "Download failed";
-        MessageBox(NULL, TEXT("http://tk-labor.iem.thm.de/bti-swt-pa-ss14/hochrechnungen.txt"), TEXT("D:\\test.txt"), MB_OK | MB_ICONERROR);
+        MessageBox(NULL, TEXT("http://tk-labor.iem.thm.de/bti-swt-pa-ss14/hochrechnungen.txt"), TEXT("C:\\Users\\Qazi\\Desktop\\Temperaturdaten.txt"), MB_OK | MB_ICONERROR);
     }
+    cout << "download finished" << endl;
 }
 
 void Subject::parse()
 {
-	int i=0;
+    unsigned int i=0, j=0;
+    Weather weatherData;
 	string line;
 	string parsedLine;
-	ifstream dataStream ("D:\\test.txt");
+    ifstream dataStream ("C:\\Users\\Qazi\\Desktop\\Temperaturdaten.txt");
 	weather tempData;
 
 	while(!dataStream.eof())
@@ -64,7 +56,10 @@ void Subject::parse()
 
 		/* return if eof is reached */
 		if(line == "")
+        {
+            cout << "parsing finished" << endl;
 			return;
+        }
 
 		/* lines with "#" are comments */
 		if(line[0] == 35)
@@ -83,9 +78,13 @@ void Subject::parse()
 		}
 
 		/* insert structure to list */
-		weather_list.insert(weather_list.end(), tempData);
-	}
+		//weather_list.insert(weather_list.end(), tempData);
+        //Weather::arrWeather[j]=tempData;
 
+        weatherData.set_weather(tempData, j);
+        //weatherData.arrWeather[j]=tempData;
+		j++;
+	}
 
 
 }
