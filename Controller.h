ad6c871aebae46f26c8301bda6db5001435b6766
Billa786 <Billa786@users.noreply.github.com>
2014-06-27 21:13:39 +0200

    Update Controller.h
    We have delete some code segments.
    We are using Qt now so we dont need to give over parameters that are actually used for the windows forms.

--- Controller.h
+++ Controller.h
@@ -1,16 +1,18 @@
-//using namespace System::Windows::Forms;
 #pragma once
 #include "Subject.h"
 #include <windows.h>
+#include "ObserverBar.h"
 
-ref class Controller
+class Controller
 {
 private:
-	Subject *subject;
+
 
 public:
-	Controller(System::Windows::Forms::DataVisualization::Charting::Chart^, System::Windows::Forms::DataVisualization::Charting::Chart^, System::Windows::Forms::Label^);
+    Subject *subject;
+
+    Controller();
 	~Controller();
-	void applyChanges(System::Windows::Forms::Label ^ labelLocations);
+	void applyChanges();
 
 };
