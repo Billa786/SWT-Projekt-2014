940b5138dd32bf27c9c665dd1a3c45fb84b6f804
Billa786 <Billa786@users.noreply.github.com>
2014-06-27 21:19:26 +0200

    Update Observer.h
    Added some includes for qwt.

--- Observer.h
+++ Observer.h
@@ -1,7 +1,16 @@
+#pragma once
+#include <qwt_plot.h>
+#include <qwt_plot_renderer.h>
+#include <qwt_plot_canvas.h>
+#include <qwt_plot_multi_barchart.h>
+#include <qwt_column_symbol.h>
+#include <qwt_plot_layout.h>
+#include <qwt_legend.h>
+#include <qwt_scale_draw.h>
 
 
 class Observer
 {
 public:
-	virtual void update() = 0;
+    virtual void update() = 0;
 };
