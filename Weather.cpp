7e063140688db816133d052bb2ba7606a0fa9b22
Billa786 <Billa786@users.noreply.github.com>
2014-07-09 16:41:53 +0200

    Update Weather.cpp
    Write our new function metaData and we use the string and the index.
    string Weather::metaData[2];--> referenz to the weather class in weather.h.

--- Weather.cpp
+++ Weather.cpp
@@ -10,4 +10,10 @@ void Weather::set_weather(weather weather_struct, int index)
     arrWeather[index]=weather_struct;
 }
 
+void Weather::set_metaData(string metaDataString, int index)
+{
+    metaData[index]=metaDataString;
+}
+
 weather Weather::arrWeather[6];
+string Weather::metaData[2];
