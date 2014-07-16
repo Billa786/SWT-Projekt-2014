aa2f0dfa2ce17756db72f6b28663663ad3187ac5
Billa786 <Billa786@users.noreply.github.com>
2014-07-16 15:40:16 +0200

    Update Weather.cpp
    Added meaningful comments which explains the two set methods.

--- Weather.cpp
+++ Weather.cpp
@@ -5,11 +5,13 @@ Weather::Weather()
 
 }
 
+/*A set method to get the data in the array*/
 void Weather::set_weather(weather weather_struct, int index)
 {
     arrWeather[index]=weather_struct;
 }
 
+/*A set method to get the metadata in the array*/
 void Weather::set_metaData(string metaDataString, int index)
 {
     metaData[index]=metaDataString;
