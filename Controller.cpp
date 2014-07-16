da7173529993c4348d1a1ebd478eb4ff8bffc2ec
Billa786 <Billa786@users.noreply.github.com>
2014-07-16 15:09:22 +0200

    Update Controller.cpp
    We added some meaningful comments.
    Also we have a new variable "successfull" to check if the download and parse work and if there are an error we can catch it.

--- Controller.cpp
+++ Controller.cpp
@@ -3,23 +3,30 @@
 #include "ObserverGraph.h"
 #include "ObserverText.h"
 
+/*The constructor creates an object of type subject and initializes and starts the timer
+  to call function applyChanges() every minute
+*/
 Controller::Controller()
 {
 	subject = new Subject();
-    	timer = new QTimer(this);
-	connect (timer, SIGNAL(timeout()), this, SLOT(applyChanges()));
-	timer->setInterval(60000);
-    	timer->start();
+    timer = new QTimer(this);
+    connect (timer, SIGNAL(timeout()), this, SLOT(applyChanges()));
+    timer->setInterval(60000);
+    timer->start();
 }
-
+/*The destructor deletes the created object*/
 Controller::~Controller()
 {
 	delete subject;
 }
-
+/*This function is called by timer and refreshs the GUI */
 void Controller::applyChanges()
 {
-	subject->download();
-	subject->parse();
+    bool successful;
+    if(!(successful = subject->download()))
+        return;
+
+    if(!(successful = subject->parse()))
+        return;
 	subject->notify();
 }
