77bcb8f6a01a5ef1cfd5b0f170c5e1da19bdded3
Billa786 <Billa786@users.noreply.github.com>
2014-07-16 15:19:29 +0200

    Update ObserverBar.h
    Changed the name of the variable d_barChartItem to barChartItem and added meanigful comments.

--- ObserverBar.h
+++ ObserverBar.h
@@ -19,3 +19,25 @@ public Q_SLOTS:
 private:
 
 };
+
+#pragma once
+#include <qwt_plot.h>
+#include "Observer.h"
+
+
+class QwtPlotMultiBarChart;
+
+/* Derived class of abstract observer class.
+   Implements update function for the barChart.
+*/
+class ObserverBar: public QwtPlot, public Observer
+{
+    Q_OBJECT
+
+public:
+    ObserverBar( QWidget * = NULL, QwtPlotMultiBarChart * = NULL);
+    void update();
+
+private:
+    QwtPlotMultiBarChart *barChartItem;
+};
