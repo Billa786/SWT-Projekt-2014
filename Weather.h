c9d156c37f344b188a654acac8c919631bb742a3
Billa786 <Billa786@users.noreply.github.com>
2014-07-16 15:39:01 +0200

    Update Weather.h
    Added meaningful comments which explains the class weather and the struct weather.

--- Weather.h
+++ Weather.h
@@ -3,13 +3,16 @@
 #include <iostream>
 using namespace std;
 
+/*Needed to save our weather data*/
 struct weather
 {
     string location;
     string temp;
 };
 
-
+/*Provides arrays for weather data to be accessable from everywhere in program.
+  set methods allow to add structs of weather data easily.
+*/
 class Weather
 {
 public:
